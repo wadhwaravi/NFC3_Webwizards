93bb2a97dc12209f3b4e2a67c29fa3e02108ba2d
wadhwaravi <117942469+wadhwaravi@users.noreply.github.com>
2024-08-30 01:53:50 +0530

    updates user profile
    user profile updates with backend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -58,7 +58,7 @@ const router = createBrowserRouter(
         {/* ==================== Header Navlink Start here =================== */}
         <Route index element={<Home />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
-        <Route path="/user-profile" element={<UserProfilePage/>}></Route>
+        <Route path="/user-profile" element={<UserProfilePage />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/journal" element={<Journal />}></Route>
@@ -70,6 +70,7 @@ const router = createBrowserRouter(
       </Route>
       <Route path="/signup" element={<SignUp />}></Route>
       <Route path="/signin" element={<SignIn />}></Route>
+      <Route path="/profile" element={<UserProfilePage />}></Route>
     </Route>
   )
 );
@@ -83,4 +84,3 @@ function App() {
 }
 
 export default App;
-

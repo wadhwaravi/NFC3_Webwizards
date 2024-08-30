1daee9110012a2969f6db2bc383f7f1e763b0a40
Jaikishaannn <kamranijaikishan1825@gmail.com>
2024-08-30 09:50:55 +0530

    product info

--- frontend/src/components/pageProps/productDetails/ProductInfo.js
+++ frontend/src/components/pageProps/productDetails/ProductInfo.js
@@ -29,15 +29,15 @@ const ProductInfo = ({ productInfo }) => {
       <h2 className="text-4xl font-semibold">{productInfo.productName}</h2>
       <p className="text-2xl font-semibold">
         {productInfo.price}
-        <span className="text-xl font-semibold line-through ml-2">540</span>
-        <span className="text-xs ml-2 inline-flex items-center px-3 py-1 rounded-full bg-green-600 text-white">
+        {/* <span className="text-xl font-semibold line-through ml-2">540</span> */}
+        {/* <span className="text-xs ml-2 inline-flex items-center px-3 py-1 rounded-full bg-green-600 text-white">
           Save 100
-        </span>
+        </span> */}
       </p>
       <hr />
       <p className="text-base text-gray-600">{renderDescription()}</p>
 
-      <div className="flex items-center">
+      {/* <div className="flex items-center">
         <p className="text-sm mr-2"> leave a review </p>
 
         <svg
@@ -85,9 +85,9 @@ const ProductInfo = ({ productInfo }) => {
         >
           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
         </svg>
-      </div>
+      </div> */}
 
-      <p className="text-base text-green-600 font-medium">En Stock</p>
+      <p className="text-base text-green-600 font-medium">In Stock</p>
       <button
         onClick={() =>
           dispatch(
@@ -106,10 +106,10 @@ const ProductInfo = ({ productInfo }) => {
       >
         Add to Cart
       </button>
-      <p className="font-normal text-sm">
+      {/* <p className="font-normal text-sm">
         <span className="text-base font-medium"> Categories:</span> Spring
         collection, Streetwear, Women Tags: featured SKU: N/A
-      </p>
+      </p> */}
     </div>
   );
 };
